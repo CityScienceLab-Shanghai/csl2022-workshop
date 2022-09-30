b514bde4b32395b819a8e12332fa0526fe65a211
LeeJAJA <jiajie@mit.edu>
2022-09-30 20:53:27 +0800

    Fixed shadows

--- src/components/map.js
+++ src/components/map.js
@@ -1,19 +1,25 @@
+import React, { useState } from "react";
+import { StaticMap } from "react-map-gl";
+import DeckGL from "@deck.gl/react";
+import { GeoJsonLayer, PolygonLayer } from "@deck.gl/layers";
+import {
+  LightingEffect,
+  AmbientLight,
+  _SunLight as SunLight,
+} from "@deck.gl/core";
+import { scaleThreshold } from "d3-scale";
 
-import React, {useState} from 'react';
-import {StaticMap} from 'react-map-gl';
-import DeckGL from '@deck.gl/react';
-import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
-import {LightingEffect, AmbientLight, _SunLight as SunLight} from '@deck.gl/core';
-import {scaleThreshold} from 'd3-scale';
-import floor_data from '../data/bld_floors.json'
+// import floor_data from "../data/bld_floors.json";
+import floor_data from "../data/processed_bld_floors.json";
 
 // Source data GeoJSON
 const DATA_URL =
-  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json'; // eslint-disable-line
-
+  "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"; // eslint-disable-line
 
 export const COLOR_SCALE = scaleThreshold()
-  .domain([-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2])
+  .domain([
+    -0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2,
+  ])
   .range([
     [65, 182, 196],
     [127, 205, 187],
@@ -28,16 +34,16 @@ export const COLOR_SCALE = scaleThreshold()
     [252, 78, 42],
     [227, 26, 28],
     [189, 0, 38],
-    [128, 0, 38]
+    [128, 0, 38],
   ]);
 
 const INITIAL_VIEW_STATE = {
   latitude: 42.35347106,
-  longitude: -71.1040540,
+  longitude: -71.094054,
   zoom: 14,
   maxZoom: 22,
   pitch: 45,
-  bearing: 0
+  bearing: 0,
 };
 
 // const INITIAL_VIEW_STATE = {
@@ -49,46 +55,46 @@ const INITIAL_VIEW_STATE = {
 //     bearing: 0
 //   };
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
 
 const ambientLight = new AmbientLight({
   color: [255, 255, 255],
-  intensity: 1.0
+  intensity: 1.0,
 });
 
 const dirLight = new SunLight({
-  timestamp: Date.UTC(2019, 7, 1, 22),
+  timestamp: Date.UTC(2019, 7, 1, 18),
   color: [255, 255, 255],
-  intensity: 1.0,
-  _shadow: true
+  intensity: 0.8,
+  _shadow: true,
 });
 
 const landCover = [
   [
-    [-123.0, 49.196],
-    [-123.0, 49.324],
-    [-123.306, 49.324],
-    [-123.306, 49.196]
-  ]
+    [-71.004054, 42.55347106],
+    [-71.004054, 42.15347106],
+    [-71.304054, 42.55347106],
+    [-71.304054, 42.25347106],
+  ],
 ];
 
-function getTooltip({object}) {
+function getTooltip({ object }) {
   return (
     object && {
       html: `\
-  <div><b>Average Property Value</b></div>
-  <div>${object.properties.valuePerParcel} / parcel</div>
-  <div>${object.properties.valuePerSqm} / m<sup>2</sup></div>
-  <div><b>Growth</b></div>
-  <div>${Math.round(object.properties.growth * 100)}%</div>
-  `
+  <div><b>ID:  </b>${object.properties.ind}</div>
+  <div><b>Floor:  </b>${object.properties.floor}</div>
+  <div><b>Category:  </b>${object.properties.Category}</div>
+  <div><b>Area:  </b>${object.properties.area}</div>
+  `,
     }
   );
 }
 
 export default function Map() {
   const [effects] = useState(() => {
-    const lightingEffect = new LightingEffect({ambientLight, dirLight});
+    const lightingEffect = new LightingEffect({ ambientLight, dirLight });
     lightingEffect.shadowColor = [0, 0, 0, 0.5];
     return [lightingEffect];
   });
@@ -96,25 +102,27 @@ export default function Map() {
   const layers = [
     // only needed when using shadows - a plane for shadows to drop on
     new PolygonLayer({
-      id: 'ground',
+      id: "ground",
       data: landCover,
       stroked: false,
-      getPolygon: f => f,
-      getFillColor: [0, 0, 0, 0]
+      getPolygon: (f) => f,
+      getFillColor: [0, 0, 0, 0],
     }),
     new GeoJsonLayer({
-      id: 'geojson',
+      id: "geojson",
       data: floor_data,
       opacity: 0.8,
       stroked: false,
-    //   filled: true,
-    //   extruded: true,
-    //   wireframe: true,
-    //   getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
-    //   getFillColor: f => COLOR_SCALE(f.properties.growth),
+      //   filled: true,
+      extruded: true,
+      //   wireframe: true,
+      //   getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
+      //   getFillColor: f => COLOR_SCALE(f.properties.growth),
+      getElevation: 1,
+      getFillColor: [237, 129, 62],
       getLineColor: [255, 255, 255],
-      pickable: true
-    })
+      pickable: true,
+    }),
   ];
 
   return (
@@ -129,4 +137,3 @@ export default function Map() {
     </DeckGL>
   );
 }
-
