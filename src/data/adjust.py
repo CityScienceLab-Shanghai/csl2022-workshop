f3c75ad5cac07531dc73b77c9d5e8502e3c40037
LeeJAJA <jiajie@mit.edu>
2022-09-30 20:53:03 +0800

    Added floor altitude fixed

---
+++ src/data/adjust.py
@@ -0,0 +1,17 @@
+import ipdb
+import json
+from tqdm import tqdm
+
+height = 20
+
+with open('./init_bld_floors.json', 'rb') as f:
+    load_json = json.load(f)
+   
+for i in tqdm(range(len(load_json['features']))):
+    for j in range(len(load_json['features'][i]['geometry']['coordinates'][0])):
+        # ipdb.set_trace()
+        load_json['features'][i]['geometry']['coordinates'][0][j][2] += height
+    
+    
+with open('./processed_bld_floors.json', 'w') as f:
+    json.dump(load_json, f)
