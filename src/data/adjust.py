a554e9af5f52fb2e05e100f8701da3345abac1bb
LeeJAJA <jiajie@mit.edu>
2022-10-06 02:06:51 +0800

    geojson fix

--- src/data/adjust.py
+++ src/data/adjust.py
@@ -4,14 +4,22 @@ from tqdm import tqdm
 
 height = 20
 
-with open('./init_bld_floors.json', 'rb') as f:
+with open('init_bld_floors.json', 'rb') as f:
     load_json = json.load(f)
-   
+
+
+# adjust floor shift       
+for i in tqdm(range(len(load_json['features']))):
+    for j in range(len(load_json['features'][i]['geometry']['coordinates'][0])):
+        # ipdb.set_trace()
+        load_json['features'][i]['geometry']['coordinates'][0][j][2] = load_json['features'][i]['geometry']['coordinates'][0][0][2]
+    
+
+# adjust height shift     
 for i in tqdm(range(len(load_json['features']))):
     for j in range(len(load_json['features'][i]['geometry']['coordinates'][0])):
         # ipdb.set_trace()
         load_json['features'][i]['geometry']['coordinates'][0][j][2] += height
     
-    
-with open('./processed_bld_floors.json', 'w') as f:
+with open('processed_bld_floors.json', 'w') as f:
     json.dump(load_json, f)
