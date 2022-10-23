9d1b4eb0ca2db2622cd683ec8ef0870529e346f6
LeeJAJA <jiajie@mit.edu>
2022-10-23 14:59:47 +0800

    Updated cover page

--- src/components/1_cover_page/CoverPage.js
+++ src/components/1_cover_page/CoverPage.js
@@ -8,26 +8,17 @@ const CoverPage = () => {
   useEffect(() => {}, []);
 
   return (
-    <div className={styles.container}>
-      <img
-        className={styles.background}
-        src={`/bg.gif`}
-        alt={""}
-      />
-      <img
-        className={styles.logo}
-        src={`/lablogo.png`}
-        alt={""}
-      />
-      <div className={styles.titleGroup}>
+    <>
+      <img className={styles.background} src={`/bg.gif`} alt={""} />
+      <img className={styles.logo} src={`/lablogo.png`} alt={""} />
+      <div className={styles.vcenter}>
+        <img className={styles.socitylogo} src={`/socity.png`} alt={""} />
         <div className={styles.title}>Community DAO</div>
+        <button className={styles.button} onClick={nextPage}>
+          <div className={styles.buttontext}>Get Started</div>
+        </button>
       </div>
-
-      <button className={styles.button} onClick={nextPage}>
-        <div className={styles.buttontext}>Get Started</div>
-      </button>
-      <div className={styles.pagination}>{page}</div>
-    </div>
+    </>
   );
 };
 
