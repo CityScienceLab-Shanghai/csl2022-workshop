422b0d8d292a76b100b79dda04744726c59b934c
LeeJAJA <jiajie@mit.edu>
2022-10-24 23:20:48 +0800

    Added bar charts

---
+++ src/components/0.4_charts/BarCharts.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import styles from "./BarCharts.module.css"
+
+const BarChart = ({ data }) => (
+  <ResponsiveBar
+    data={data}
+    keys={["value"]}
+    indexBy="type"
+    margin={{ top: 30, right: 30, bottom: 50, left: 60 }}
+    padding={0.3}
+    minValue={0}
+    maxValue={1}
+    valueScale={{ type: "linear" }}
+    indexScale={{ type: "band", round: true }}
+    valueFormat=" >-.4~%"
+    colors={["rgb(234, 76, 111, 0.2)", "rgb(36, 83, 255, 0.2)"]}
+    colorBy="indexValue"
+    isInteractive={false}
+    borderWidth={2}
+    borderColor={{ from: "color"}}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      // legend: "country",
+      // legendPosition: "middle",
+      legendOffset: 32,
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      // legend: "food",
+      // legendPosition: "middle",
+      legendOffset: -40,
+    }}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor="#ffffff"
+    motionConfig="stiff"
+  />
+);
+
+export default BarChart;
--- src/components/2.2_voting/IndicatorCard.js
+++ src/components/2.2_voting/IndicatorCard.js
@@ -1,9 +1,11 @@
 import styles from "./IndicatorCard.module.css";
 
+import BarChart from "../0.4_charts/BarCharts"
+import _BAR_DATA from "../../data/charts/bar_chart.json"
 const IndicatorCard = () => {
   return (
     <div className={styles.card}>
-      
+      <BarChart data={_BAR_DATA}/>
     </div>
   );
 };
