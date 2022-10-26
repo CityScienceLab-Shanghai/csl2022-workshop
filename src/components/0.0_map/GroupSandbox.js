60301f187be949664b456bf2842554f64412938e
LeeJAJA <jiajie@mit.edu>
2022-10-26 19:00:50 +0800

    added double scroll

--- src/components/0.0_map/GroupSandbox.js
+++ src/components/0.0_map/GroupSandbox.js
@@ -23,8 +23,56 @@ import CustomButton from "../0.1_buttons/CustomButton";
 import _CONTENT from "../../data/sandbox/sandbox_card_content.json";
 
 import Slider from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
 
 import { stateStore } from "../../stores";
+
+const CustomSlider = styled(Slider)(({ theme }) => ({
+  color: "#EA4C6F",
+  height: 3,
+  padding: "13px 0",
+  "& .MuiSlider-thumb": {
+    height: 20,
+    width: 20,
+    backgroundColor: "#2f0f16 ",
+    border: "1px solid #ea4c6f ",
+    "&:hover": {
+      boxShadow: "0 0 0 8px rgba(234, 76, 111, 0.16)",
+    },
+    "& .airbnb-bar": {
+      height: 9,
+      width: 1,
+      backgroundColor: "currentColor",
+      marginLeft: 1,
+      marginRight: 1,
+    },
+  },
+  "& .MuiSlider-valueLabel": {
+    marginTop: "3px",
+    background: "#cecece",
+    borderRadius: "2px",
+
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: "10px",
+
+    color: "rgba(0, 0, 0, 0.75)",
+
+    paddingTop: "3px",
+    paddingRight: "6px",
+    paddingBottom: "2px",
+    paddingLeft: "6px",
+  },
+  "& .MuiSlider-track": {
+    height: 3,
+  },
+  "& .MuiSlider-rail": {
+    color: "#2D2D2D",
+    opacity: 1,
+    height: 3,
+  },
+}));
 
 const GroupSandbox = () => {
   const { barCharts, updateBarCharts } = stateStore;
@@ -139,24 +187,23 @@ const GroupSandbox = () => {
 
     return (
       <div className={styles.buttonGroup}>
-        <div className={styles.slider}>
-          <Slider
-            min={0}
-            max={10}
-            value={simple_sandbox_slider_value_1}
-            onChange={(event, newValue) => {
-              set_simple_sandbox_slider_value_1(newValue);
-            }}
-            step={1}
-            marks
-            valueLabelDisplay="on"
-            valueLabelFormat={valueLabelFormat}
-            slotProps={{
-              valueLabel: { className: styles.valueLabel },
-              thumb: { className: styles.sliderThumb },
-            }}
-          />
-          <Slider
+        <div className={styles.sliderBox}>
+          <div className={styles.sliderItem}>
+            <CustomSlider
+              min={0}
+              max={10}
+              value={simple_sandbox_slider_value_1}
+              onChange={(event, newValue) => {
+                set_simple_sandbox_slider_value_1(newValue);
+              }}
+              step={1}
+              marks
+              valueLabelDisplay="on"
+              valueLabelFormat={valueLabelFormat}
+            />
+          </div>
+
+          <CustomSlider
             min={0}
             max={10}
             value={simple_sandbox_slider_value_2}
@@ -167,10 +214,6 @@ const GroupSandbox = () => {
             marks
             valueLabelDisplay="on"
             valueLabelFormat={valueLabelFormat}
-            slotProps={{
-              valueLabel: { className: styles.valueLabel },
-              thumb: { className: styles.sliderThumb },
-            }}
           />
         </div>
       </div>
