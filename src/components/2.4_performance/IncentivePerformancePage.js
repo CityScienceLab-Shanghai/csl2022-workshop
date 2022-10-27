2a6af7149f9067658e8f89cc928c1fa40bf70e1a
LeeJAJA <jiajie@mit.edu>
2022-10-28 03:26:32 +0800

    Added 2.4

--- src/components/2.4_performance/IncentivePerformancePage.js
+++ src/components/2.4_performance/IncentivePerformancePage.js
@@ -17,8 +17,42 @@ const IncentivePerformancePage = () => {
         </div>
         <div className={styles.rowGroup} style={{ marginTop: "30px" }}>
           <div className={styles.description}>
-          This is the comparison between the current practice and community DAO paradigm.
+            This is the comparison between the current practice and community
+            DAO paradigm.
           </div>
+        </div>
+
+        <div className={styles.rowGroup} style={{ marginTop: "50px" }}>
+          <div className={styles.colGroup}>
+            <div className={styles.title_current}>Current Practice</div>
+            <div className={styles.resultContent}>
+              Development based on the right zoning with 20% affordable housing
+              required by the Inclusionary Housing Program.
+            </div>
+            <div className={styles.resultContent}>- 5 floors</div>
+          </div>
+          <img
+            className={styles.img}
+            src={`/building_rendered/result_1.png`}
+            alt={""}
+          />
+        </div>
+
+        <div className={styles.rowGroup} style={{ marginTop: "50px" }}>
+          <div className={styles.colGroup}>
+            <div className={styles.title_DAO}>Current Practice</div>
+            <div className={styles.resultContent}>
+              Five extra floors adding on to the right zoning, with 70% of the
+              profit donated to the community encdowment.
+            </div>
+            <div className={styles.resultContent}>- 5+5=10 floors</div>
+            <div className={styles.resultContent}>- 70% projected profit for community endowment</div>
+          </div>
+          <img
+            className={styles.img}
+            src={`/building_rendered/result_2.png`}
+            alt={""}
+          />
         </div>
         <div style={{ marginTop: "62px", marginBottom: "92px" }}></div>
         <IndicatorCard />
